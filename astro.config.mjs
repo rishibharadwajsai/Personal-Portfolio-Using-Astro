9d49636d4ec663550504f647fd8eeaa628224d6e
Rishi Bharadwaj Sai Lella <rishi.lella123@gmail.com>
2024-08-11 15:52:42 +0530

    Revert "Added vercel analytics"
    This reverts commit fa9002ac8338745f2a6f82f2ee04e2f876694232.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
   site: 'https://rishibharadwajsai.vercel.app',
   integrations: [tailwind(), sitemap()]
 });
